d8947194a9b28c2a118483cc616aa63cf49491a1
Troy Goode <troygoode@gmail.com>
2013-04-28 13:35:03 -0400

    WIP

--- app.js
+++ app.js
@@ -3,12 +3,29 @@
 'use strict';
 
 var express = require('express'),
+    cors = require('cors'),
     port = process.env.PORT || 3000,
     app = express();
 
-app.get('/example', function(req, res){
+app.get('/no-cors', function(req, res){
   res.json({
-    text: 'This is an example.'
+    text: 'You should not see this via a CORS request.'
+  });
+});
+
+app.get('/simple-cors', cors(), function(req, res){
+  res.json({
+    text: 'Simple CORS requests are working. [GET]'
+  });
+});
+app.head('/simple-cors', cors(), function(req, res){
+  res.json({
+    text: 'Simple CORS requests are working. [HEAD]'
+  });
+});
+app.post('/simple-cors', cors(), function(req, res){
+  res.json({
+    text: 'Simple CORS requests are working. [POST]'
   });
 });
 
