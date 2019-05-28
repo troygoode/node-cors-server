18da95346359cd974648033908cda4c2c618c486
Troy Goode <troygoode@gmail.com>
2019-05-27 21:25:04 -0700

    a bit of modernization

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-node-cors-server
-================
+# node-cors-server
+
+A test application that helps illustrate CORS while both in a working state and a non-working state across simple and complex request scenarios.
+
+## See Also
+
+* https://github.com/expressjs/cors
+* https://github.com/troygoode/node-cors-client
+* https://github.com/troygoode/node-cors-server
+
+## License
+
+[MIT License](http://www.opensource.org/licenses/mit-license.php)
+
+## Author
+
+[Troy Goode](https://github.com/troygoode) ([troygoode@gmail.com](mailto:troygoode@gmail.com))
---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+/* -------------------------------------------------------------------------- */
+
+app.get("/no-cors", function(req, res) {
+  res.json({
+    text: "You should not see this via a CORS request."
+  });
+});
+
+/* -------------------------------------------------------------------------- */
+
+app.get("/simple-cors", cors(), function(req, res) {
+  res.json({
+    text: "Simple CORS requests are working. [GET]"
+  });
+});
+app.head("/simple-cors", cors(), function(req, res) {
+  res.send(204);
+});
+app.post("/simple-cors", cors(), function(req, res) {
+  res.json({
+    text: "Simple CORS requests are working. [POST]"
+  });
+});
+
+/* -------------------------------------------------------------------------- */
+
+app.options("/complex-cors", cors());
+app.delete("/complex-cors", cors(), function(req, res) {
+  res.json({
+    text: "Complex CORS requests are working. [DELETE]"
+  });
+});
+
+/* -------------------------------------------------------------------------- */
+
+var issue2options = {
+  origin: true,
+  methods: ["POST"],
+  credentials: true,
+  maxAge: 3600
+};
+app.options("/issue-2", cors(issue2options));
+app.post("/issue-2", cors(issue2options), function(req, res) {
+  res.json({
+    text: "Issue #2 is fixed."
+  });
+});
+
+/* -------------------------------------------------------------------------- */
+
+if (!module.parent) {
+  const port = process.env.PORT || 3000;
+
+  app.listen(port, function() {
+    console.log("Express server listening on port " + port + ".");
+  });
+}
